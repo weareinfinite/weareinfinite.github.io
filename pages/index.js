eb8ccb7b4d39e4dcfac117935fc27e57614278a1
Sarath <saratonite@gmx.com>
2017-12-14 18:04:42 +0530

    :cake: Fixed SVG Logo attribute issues, box tilt effect , text type effect :fire:

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
+import React from 'react';
 import Meta from '../components/meta'
 import Logo from '../components/logo'
-export default () => (
-    <div>
-        <div className="thebox">
-            <Meta/>
-            <Logo/>
-            weareinfinite
+import Typist from 'react-typist';
+import tilt from 'vanilla-tilt';
+export default class Home extends React.Component {
+
+    componentDidMount() {
+
+        tilt.init(this.thebox, {
+            speed:200,
+            max:35
+        })
+        
+
+    }
+
+    render = () => (
+        <div>
+            <div className="thebox" ref={ element => this.thebox = element }>
+                <Meta/>
+                <Logo/>
+                weareinfinite
+            </div>
+            <div className="about__box">
+                
+                <Typist>
+                Every product is designed and engineered by the solo-founder Sarath
+                </Typist>
+            </div>
+           
         </div>
-        <div className="about__box">
-            <p>Every product is designed and engineered by the solo-founder Sarath</p>
-        </div>
-       
-    </div>
-)
+    )
+} 
+  
