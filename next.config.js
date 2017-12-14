b7800ae08ac7145dcff7dad1e083eb54d90cb36b
Sarath <saratonite@gmx.com>
2017-12-14 18:52:54 +0530

    :crown: Fixed ghpages asset loading issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+    assetPrefix: isProd ? 'https://raw.githubusercontent.com/weareinfinite/weareinfinite.github.io/master' : '',
     webpack:(config, {buildId, dev}) => {
         
           return config
@@ -8,5 +11,5 @@ module.exports = {
         return {
             '/': { page: '/'}
         }
-    }
+    },
 }
